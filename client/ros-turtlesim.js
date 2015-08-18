2289a73f5fd36fe04a8bc6806ceb6f1c395a4500
savalos <santas.klaus@gmail.com>
2015-08-18 16:43:53 -0300

    new infrastructure, fixed libs

---
+++ client/ros-turtlesim.js
@@ -0,0 +1,79 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+// Connecting to ROS
+// -----------------
+var ros = new ROSLIB.Ros();
+// If there is an error on the backend, an 'error' emit will be emitted.
+ros.on('error', function(error) {
+  document.getElementById('connecting').style.display = 'none';
+  document.getElementById('connected').style.display = 'none';
+  document.getElementById('closed').style.display = 'none';
+  document.getElementById('error').style.display = 'inline';
+  console.log(error);
+});
+// Find out exactly when we made a connection.
+ros.on('connection', function() {
+  console.log('Connection made!');
+  document.getElementById('connecting').style.display = 'none';
+  document.getElementById('error').style.display = 'none';
+  document.getElementById('closed').style.display = 'none';
+  document.getElementById('connected').style.display = 'inline';
+});
+ros.on('close', function() {
+  console.log('Connection closed.');
+  document.getElementById('connecting').style.display = 'none';
+  document.getElementById('connected').style.display = 'none';
+  document.getElementById('closed').style.display = 'inline';
+});
+// Create a connection to the rosbridge WebSocket server.
+ros.connect('ws://localhost:9090');
+
+// Connect with turtle position, in order to draw movements 
+var i = 0;
+var posE = new ROSLIB.Topic({
+  ros : ros,
+  name : '/turtle1/pose',
+  messageType : 'turtlesim/Pose'
+});
+posE.subscribe(function(message) {
+  //console.log(i + ' - Received message on ' + posE.name + ': ' + JSON.stringify(message));
+  i++;
+});
+//----------------------
+
+// Connect with turtle movement topic, in order to push some control vectors
+var turtle_cmdVel = new ROSLIB.Topic({
+  ros : ros,
+  name : '/turtle1/cmd_vel',
+  messageType : 'geometry_msgs/Twist'
+});
+// payload
+var twist = new ROSLIB.Message({
+  linear : {
+    x : 2.0,
+    y : 0.0,
+    z : 0.0
+  },
+  angular : {
+    x : 0.0,
+    y : 0.0,
+    z : 0.0
+  }
+});
+turtle_cmdVel.publish(twist);
