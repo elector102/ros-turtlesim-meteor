4f6f20003e3d69df6415d3929d05ee68f7d9f7be
Luis Bosch <luisbosch@gmail.com>
2015-08-19 14:42:06 -0300

    Added json configuration

--- client/controllers/connectRos.js
+++ client/controllers/connectRos.js
@@ -2,7 +2,7 @@
 // -----------------
 ros = new ROSLIB.Ros();
 // If there is an error on the backend, an 'error' emit will be emitted.
-ros.on('error', function(error) {
+ros.on('error', function (error) {
   document.getElementById('connecting').style.display = 'none';
   document.getElementById('connected').style.display = 'none';
   document.getElementById('closed').style.display = 'none';
@@ -10,18 +10,28 @@ ros.on('error', function(error) {
   console.log(error);
 });
 // Find out exactly when we made a connection.
-ros.on('connection', function() {
+ros.on('connection', function () {
   console.log('Connection made!');
   document.getElementById('connecting').style.display = 'none';
   document.getElementById('error').style.display = 'none';
   document.getElementById('closed').style.display = 'none';
   document.getElementById('connected').style.display = 'inline';
 });
-ros.on('close', function() {
+ros.on('close', function () {
   console.log('Connection closed.');
   document.getElementById('connecting').style.display = 'none';
   document.getElementById('connected').style.display = 'none';
   document.getElementById('closed').style.display = 'inline';
 });
 // Create a connection to the rosbridge WebSocket server.
-ros.connect('ws://192.168.0.105:9090');
+
+var connectionUrl = 'ws://localhost:9090';
+
+$.getJSON('config.json', function (config) {
+  if (config) {
+    connectionUrl = config.connectionUrl;
+  };
+  console.log(connectionUrl);
+  ros.connect(connectionUrl);  
+});
+
